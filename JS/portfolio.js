e21b110d1aeef300a72b17d3565a03e148c7a44d
Prasath <pprasathpp@gmail.com>
2021-02-05 02:30:06 +0100

    updated
    About me and styling done

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -4,9 +4,15 @@ $(document).ready(function(){
             $('.navbar').addClass("sticky");
         }
         else{
-            $('.navbar').removeClass("sticky")
+            $('.navbar').removeClass("sticky");
 
         }
+    });
+
+    // toggle menu/navbar script //
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
     })
 
 })
